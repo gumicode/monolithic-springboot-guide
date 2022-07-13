2a9386d481c8abcbf4ed22087f8ffa52a287a09a
gumicode <gumicode@gmail.com>
2022-07-13 22:21:42 +0900

    초기데이터 생성 클래스 추가

---
+++ src/main/java/com/example/monolithic/DummyDataInitializer.java
@@ -0,0 +1,41 @@
+package com.example.monolithic;
+
+import com.example.monolithic.domain.member.entity.Member;
+import com.example.monolithic.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class DummyDataInitializer implements ApplicationRunner {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public void run(ApplicationArguments args) {
+
+
+		initialMember();
+
+	}
+
+
+	private void initialMember() {
+
+		// given
+		List<Member> members = Stream.iterate(0, n -> n + 1)
+				.limit(100)
+				.map(t -> Member.builder()
+						.username("username" + t)
+						.password("abcd1234!")
+						.build()).toList();
+
+		// when
+		memberRepository.saveAll(members);
+	}
+}
